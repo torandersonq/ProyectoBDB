b707b6f4e859e7178d5581fef2443169af244d0a
Anderson Y. Torres Q. <torandersonq@gmail.com>
2021-05-07 23:10:23 -0500

    Se crea proyecto con la configuracion inicar en en el .web para los servicios rest

---
+++ ProyectoBDB-web/src/main/java/com/gmail/torandersonq/controlador/EmpleadoControlador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmail.torandersonq.controlador;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Ander
+ */
+
+@Stateless // No maneja comunicacion abierta
+@Path("/empleados") // Entrada URL para servicio empleado
+public class EmpleadoControlador {
+
+    
+}
